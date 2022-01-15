9d88ee81718d1f1f91d61697f8571687ffeea92f
leon-marx <gooogleon@gmail.com>
2022-01-15 17:18:47 +0100

    changed callback to save images

--- callbacks/image_logger.py
+++ callbacks/image_logger.py
@@ -5,9 +5,14 @@ import torchvision
 
 
 class ImageLogger(Callback):
+    def __init__(self, out_dir):
+        super().__init__()
+        self.out_dir = out_dir
+
     def on_train_batch_end(self, trainer, pl_module, outputs, batch, batch_idx, unused=0):
         self.train_batch = batch
         return super().on_train_batch_end(trainer, pl_module, outputs, batch, batch_idx, unused)
+
     def on_validation_batch_end(self, trainer, pl_module, outputs, batch, batch_idx, dataloader_idx):
         self.val_batch = batch
         return super().on_validation_batch_end(trainer, pl_module, outputs, batch, batch_idx, dataloader_idx)
@@ -21,15 +26,14 @@ class ImageLogger(Callback):
             train_contents = self.train_batch[2]
             train_recs = pl_module(train_imgs, train_domains, train_contents, raw=True)[2]
             train_grid = torchvision.utils.make_grid(torch.cat((train_imgs, train_recs), 0))
-            trainer.logger.experiment.add_image("train_images", train_grid, 0)
+            torchvision.utils.save_image(train_grid, f"{self.out_dir}/images/train_reconstructions.png")
 
             val_imgs = self.val_batch[0]
             val_domains = self.val_batch[1]
             val_contents = self.val_batch[2]
             val_recs = pl_module(val_imgs, val_domains, val_contents, raw=True)[2]
             val_grid = torchvision.utils.make_grid(torch.cat((val_imgs, val_recs), 0))
-            trainer.logger.experiment.add_image("val_images", val_grid, 0)
+            torchvision.utils.save_image(val_grid, f"{self.out_dir}/images/val_reconstructions.png")
 
             pl_module.train()
         return super().on_save_checkpoint(trainer, pl_module, checkpoint)
-        
