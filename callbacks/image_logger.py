f51ceea92f082e7422ba89c0b692cdccb0f515b0
leon-marx <gooogleon@gmail.com>
2022-01-16 13:55:09 +0100

    bug fix

--- callbacks/image_logger.py
+++ callbacks/image_logger.py
@@ -20,6 +20,7 @@ class ImageLogger(Callback):
             pl_module.eval()
             os.makedirs(f"{self.out_dir}/images", exist_ok=True)
 
+            self.train_batch = self.train_batch.to(pl_module.device)
             train_imgs = self.train_batch[0]
             train_domains = self.train_batch[1]
             train_contents = self.train_batch[2]
@@ -27,6 +28,7 @@ class ImageLogger(Callback):
             train_grid = torchvision.utils.make_grid(torch.cat((train_imgs, train_recs), 0))
             torchvision.utils.save_image(train_grid, f"{self.out_dir}/images/train_reconstructions.png")
 
+            self.val_batch = self.val_batch.to(pl_module.device)
             val_imgs = self.val_batch[0]
             val_domains = self.val_batch[1]
             val_contents = self.val_batch[2]
