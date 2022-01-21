f3b791b74efd68e0ff1a7658b83382a795726d42
leon-marx <gooogleon@gmail.com>
2022-01-22 00:37:22 +0100

    debugging

--- tools/sweep.py
+++ tools/sweep.py
@@ -32,13 +32,11 @@ def get_combinations(arg_dict):
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument("--restart", type=bool, default=False)
+    parser.add_argument("--restart", action="store_true", default=False)
     parser.add_argument("--gpus", type=str, default="3,")
     parser.add_argument("--max_epochs", type=int, default=25)
-    parser.add_argument("--iov", type=bool, default=True)
+    parser.add_argument("--iov", action="store_true", default=True)
     args = parser.parse_args()
-    args_restart = args.restart == "True"
-    args_iov = args.iov == "True"
     #################### EDIT THIS IN ORDER TO CHANGE THE SWEEP
     configs = {
         # "AE_v2": {
@@ -151,11 +149,11 @@ if __name__ == "__main__":
 
                 
 
-            if args_restart or not os.path.isdir(f"{log_dir}"):
+            if args.restart or not os.path.isdir(f"{log_dir}"):
                 # Configuration
                 os.makedirs(log_dir, exist_ok=True)
                 callbacks = [
-                    Logger(log_dir, train_batch, val_batch, images_on_val=args_iov)
+                    Logger(log_dir, train_batch, val_batch, images_on_val=args.iov)
                 ]
                 
                 print("Args:")
