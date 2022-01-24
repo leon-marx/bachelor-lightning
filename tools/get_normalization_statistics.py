6215c856464ed5bed7e685eb459f7aba44109bd7
leon-marx <gooogleon@gmail.com>
2022-01-24 02:38:17 +0100

    bug fix

--- tools/get_normalization_statistics.py
+++ tools/get_normalization_statistics.py
@@ -2,32 +2,46 @@ import torch
 from tqdm import tqdm
 from datasets import pacs
 from datasets.pacs import PACSDataModule
+from argparse import ArgumentParser
+
 
 
 if __name__ == "__main__":
-    domains = ["art_painting", "cartoon", "photo", "sketch"]
+    parser = ArgumentParser()
+    parser.add_argument("--domain", type=str)
+    args = parser.parse_args()
+    # domains = ["art_painting", "cartoon", "photo", "sketch"]
     gather_data = True
     batch_size = 1
     N = 50
 
     if gather_data:
-        for domain in domains:
-            dm = PACSDataModule(
-                root="data", 
-                domains= [domain],
-                contents=["dog", "elephant", "giraffe", "guitar", "horse", "house", "person"],
-                batch_size=batch_size,
-                num_workers=20,
-                normalize=False)
-            dm.setup()
-            true_total = torch.zeros(size=(len(dm.train_dataloader()) * N, 3, 224, 224))
-            for i in tqdm(range(N)):
-                for j, batch in enumerate(tqdm(dm.train_dataloader(), leave=False)):
-                    true_total[i * N + j] = batch[0]
-            torch.save(true_total, f"logs/{domain}_statistics.pt")
-    for domain in domains:
-        tot = torch.load(f"logs/{domain}_statistics.pt")
-        print(domain)
-        print("mean:", tot.mean(dim=[0, 2, 3]))
-        print("std:", tot.std(dim=[0, 2, 3]))
+        dm = PACSDataModule(
+            root="data", 
+            domains= [args.domain],
+            contents=["dog", "elephant", "giraffe", "guitar", "horse", "house", "person"],
+            batch_size=batch_size,
+            num_workers=20,
+            normalize=False)
+        dm.setup()
+
+        mean = 0.0
+        std = 0.0
+        for i in tqdm(range(N)):
+            channels_sum, channels_squared_sum, num_batches = 0.0, 0.0, 0.0
+            for batch in tqdm(dm.train_dataloader(), leave=False):
+                # Mean over batch, height and width, but not over the channels
+                channels_sum += torch.mean(batch[0], dim=[0,2,3])
+                channels_squared_sum += torch.mean(batch[0]**2, dim=[0,2,3])
+                num_batches += 1
+        
+            mean += channels_sum / num_batches / N
+            # std = sqrt(E[X^2] - (E[X])^2)
+            std += (channels_squared_sum / num_batches - mean ** 2) ** 0.5 / N
+
+        torch.save(mean, f"logs/{args.domain}_mean_statistics.pt")
+        torch.save(std, f"logs/{args.domain}_std_statistics.pt")
+        print(args.domain)
+        print("mean:", mean)
+        print("std:", std)
         print("")
