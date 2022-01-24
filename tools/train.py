66408ed95c2168b5f64bebdb69a0a976bc41b464
leon-marx <gooogleon@gmail.com>
2022-01-24 11:51:28 +0100

    changed grad clipping to norm and the threshold to 1

--- tools/train.py
+++ tools/train.py
@@ -171,8 +171,8 @@ if __name__ == "__main__":
         logger=pl.loggers.TensorBoardLogger(save_dir=os.getcwd(),
                                             name=args.output_dir),
         callbacks=callbacks,
-        gradient_clip_val=0.5,
-        gradient_clip_algorithm="value",
+        gradient_clip_val=1.0,
+        gradient_clip_algorithm="norm",
         max_epochs=args.max_epochs,
         enable_checkpointing=args.enable_checkpointing,
         log_every_n_steps=args.log_every_n_steps
