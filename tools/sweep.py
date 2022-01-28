b40051466369def692c0cf3655e0e56d09be5ecf
leon-marx <gooogleon@gmail.com>
2022-01-28 11:06:42 +0100

    added trvae and dc cvae to sweep

--- tools/sweep.py
+++ tools/sweep.py
@@ -8,10 +8,13 @@ import random
 
 # Own Modules
 from datasets.pacs import PACSDataModule
+from datasets.pacs_balanced import BalancedPACSDataModule
 from models.cvae import CVAE
 from models.ae import AE
 from models.ae_v2 import AE_v2
 from models.ae_v3 import AE_v3
+from models.dccvae import DCCVAE
+from models.trvae import trVAE
 from callbacks.logger import Logger
 
 
@@ -49,11 +52,26 @@ if __name__ == "__main__":
         #     "latent_size": [128, 512],
         #     "lamb": [0.01, 100],
         # },
-        "AE_v3": {
-            "kernel_size": [3, 5],
-            "activation": ["selu", "elu", "relu"],
-            "loss_mode": ["l1", "l2"]
+        # "AE_v3": {
+        #     "kernel_size": [3, 5],
+        #     "activation": ["selu", "elu", "relu"],
+        #     "loss_mode": ["l1", "l2"]
+        # },
+        "DC_CVAE": {
+            "latent_size": [128, 512, 1024],
+            "feature_size": [32, 64, 128],
+            "lr": [1e-3, 1e-4, 1e-5],
+            "lamb": [1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 1.0, 1e+1],
         },
+        "trVAE": {
+            "latent_size": [128, 512, 1024],
+            "feature_size": [32, 64],
+            "mmd_size": [512, 1024],
+            "dropout_rate": [0.0, 0.3, 0.5],
+            "lr": [1e-3, 1e-4, 1e-5],
+            "lamb": [1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 1.0, 1e+1],
+            "beta": [1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 1.0, 1e+1],
+            },
     }
     ####################
     # Configuration
@@ -70,10 +88,10 @@ if __name__ == "__main__":
     # Dataset
     domains = ["art_painting", "cartoon", "photo"]
     contents = ["dog", "elephant", "giraffe", "guitar", "horse", "house", "person"]
-    batch_size = 8
-    dm = PACSDataModule(root="data/variants/PACS_small", domains=domains, contents=contents,
+    batch_size = 4
+    dm = BalancedPACSDataModule(root="data/variants/PACS_small", domains=domains, contents=contents,
                         batch_size=batch_size, num_workers=20)
-    log_dm = PACSDataModule(root="data/variants/PACS_small", domains=domains, contents=contents,
+    log_dm = BalancedPACSDataModule(root="data/variants/PACS_small", domains=domains, contents=contents,
                         batch_size=batch_size, num_workers=20, shuffle_all=True)
     num_domains = len(domains)
     num_contents = len(contents)
@@ -93,7 +111,7 @@ if __name__ == "__main__":
             # Default values
             log_dir = f"logs/sweep/{model_name}"
             latent_size =  128
-            lamb =  10
+            lamb =  1.0
             lr =  1e-4
             depth = 1
             out_channels = "128,128,256,256,512,512"
@@ -104,16 +122,36 @@ if __name__ == "__main__":
             dropout = False
             batch_norm = True
             loss_mode = "l1"
+            feature_size = 32
+            mmd_size = 512
+            beta = 1.0
+            dropout_rate = 0.0
 
 
             if "latent_size" in conf:
                 latent_size = conf["latent_size"]
                 log_dir += f"_{latent_size}"
+            if "feature_size" in conf:
+                feature_size = conf["feature_size"]
+                log_dir += f"_{feature_size}"
+            if "mmd_size" in conf:
+                feature_size = conf["mmd_size"]
+                log_dir += f"_{mmd_size}"
             if "lamb" in conf:
                 lamb = conf["lamb"]
                 lamb_string = "{:e}".format(lamb)
                 lamb_string = lamb_string.replace(".", "-")
                 log_dir += f"_{lamb_string}"
+            if "beta" in conf:
+                beta = conf["beta"]
+                beta_string = "{:e}".format(beta)
+                beta_string = beta_string.replace(".", "-")
+                log_dir += f"_{beta_string}"
+            if "dropout_rate" in conf:
+                dropout_rate = conf["dropout_rate"]
+                dropout_rate_string = "{:e}".format(dropout_rate)
+                dropout_rate_string = dropout_rate_string.replace(".", "-")
+                log_dir += f"_{dropout_rate_string}"
             if "lr" in conf:
                 lr = conf["lr"]
                 lr_string = "{:e}".format(lr)
@@ -186,6 +224,13 @@ if __name__ == "__main__":
                                 latent_size=latent_size, lr=lr, depth=depth, out_channels=out_channels, 
                                 kernel_size=kernel_size, activation=activation, downsampling=downsampling, 
                                 upsampling=upsampling, dropout=dropout, batch_norm=batch_norm, loss_mode=loss_mode)
+                if args.model == "DCCVAE":
+                    model = DCCVAE(num_domains=num_domains, num_contents=num_contents, lr=lr,
+                                latent_size=latent_size, feature_size=feature_size, loss_mode=loss_mode, lamb=lamb)
+                if args.model == "trVAE":
+                    model = trVAE(num_domains=num_domains, num_contents=num_contents, latent_size=latent_size,
+                                feature_size=feature_size, mmd_size=mmd_size, dropout_rate=dropout_rate,
+                                lr=lr, lamb=lamb, beta=beta)
 
 
                 # Trainer
@@ -201,7 +246,7 @@ if __name__ == "__main__":
                     gradient_clip_algorithm="value",
                     max_epochs=args.max_epochs,
                     enable_checkpointing=False,
-                    log_every_n_steps=1
+                    log_every_n_steps=5
                 )
 
                 # Main
