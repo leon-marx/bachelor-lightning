13024afc00d75c6c12142534f9ea500adbae76d4
leon-marx <gooogleon@gmail.com>
2022-01-28 11:11:00 +0100

    bug fix

--- tools/sweep.py
+++ tools/sweep.py
@@ -38,7 +38,7 @@ if __name__ == "__main__":
     parser.add_argument("--restart", action="store_true", default=False)
     parser.add_argument("--gpus", type=str, default="3,")
     parser.add_argument("--max_epochs", type=int, default=25)
-    parser.add_argument("--iov", action="store_true", default=True)
+    parser.add_argument("--no_iov", action="store_false", default=True)
     args = parser.parse_args()
     #################### EDIT THIS IN ORDER TO CHANGE THE SWEEP
     configs = {
@@ -191,7 +191,7 @@ if __name__ == "__main__":
                 # Configuration
                 os.makedirs(log_dir, exist_ok=True)
                 callbacks = [
-                    Logger(log_dir, train_batch, val_batch, images_on_val=args.iov)
+                    Logger(log_dir, train_batch, val_batch, domains, contents, images_on_val=args.no_iov)
                 ]
                 
                 print("Args:")
