55a3f76f5a5e14c170dc056370ebf245cef46496
leon-marx <gooogleon@gmail.com>
2022-01-29 23:51:48 +0100

    debugging

--- callbacks/logger.py
+++ callbacks/logger.py
@@ -41,6 +41,7 @@ class Logger(Callback):
 
     def on_train_batch_end(self, trainer, pl_module, outputs, batch, batch_idx, unused=0):
         self.gather_grad_flow_data(pl_module)
+        print(outputs)
         self.train_loss.append(outputs["loss"].item())
 
         return super().on_train_batch_end(trainer, pl_module, outputs, batch, batch_idx, unused)
