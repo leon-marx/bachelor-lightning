70f8084dca89a7026947b88420a164b1b301189f
leon-marx <gooogleon@gmail.com>
2022-02-03 23:17:49 +0100

    bug fix

--- tools/train.py
+++ tools/train.py
@@ -168,9 +168,9 @@ if __name__ == "__main__":
                         batch_norm=batch_norm)
             current_model_dict = model.state_dict()
             loaded_state_dict = torch.load(args.ckpt_path)["state_dict"]
-            new_state_dict={k:v if v.size()==current_model_dict[k].size()  else  current_model_dict[k] for k,v in zip(current_model_dict.keys(), loaded_state_dict.values())}
+            new_state_dict={k:v.cpu() if v.size()==current_model_dict[k].size()  else  current_model_dict[k] for k,v in zip(current_model_dict.keys(), loaded_state_dict.values())}
             model.load_state_dict(new_state_dict, strict=False)
-            model.to(f"cuda:{args.gpus[0]}")
+            #model.to(f"cuda:{args.gpus[0]}")
             # model = AAE.load_from_checkpoint(args.ckpt_path, num_domains=num_domains, num_contents=num_contents,
             #             latent_size=latent_size, lr=lr, depth=depth, 
             #             out_channels=out_channels, kernel_size=kernel_size, activation=activation,
