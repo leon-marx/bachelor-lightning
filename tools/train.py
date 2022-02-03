a38e500c1ddd1d609dffb7796db9a55002b64254
leon-marx <gooogleon@gmail.com>
2022-02-03 21:19:55 +0100

    bug f

--- tools/train.py
+++ tools/train.py
@@ -167,7 +167,7 @@ if __name__ == "__main__":
                         downsampling=downsampling, upsampling=upsampling, dropout=dropout,
                         batch_norm=batch_norm)
             current_model_dict = model.state_dict()
-            loaded_state_dict = torch.load(args.ckpt_path)[".state_dict"]
+            loaded_state_dict = torch.load(args.ckpt_path)["state_dict"]
             new_state_dict={k:v if v.size()==current_model_dict[k].size()  else  current_model_dict[k] for k,v in zip(current_model_dict.keys(), loaded_state_dict.values())}
             model.load_state_dict(new_state_dict, strict=False)
             # model = AAE.load_from_checkpoint(args.ckpt_path, num_domains=num_domains, num_contents=num_contents,
