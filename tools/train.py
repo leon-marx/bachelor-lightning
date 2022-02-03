531aced747e6b00bb2ebed904b13d1040bc111f1
leon-marx <gooogleon@gmail.com>
2022-02-03 21:13:35 +0100

    added option to load AAE from checkpoint with unmatching sizes

--- tools/train.py
+++ tools/train.py
@@ -161,11 +161,20 @@ if __name__ == "__main__":
                         feature_size=feature_size, mmd_size=mmd_size, dropout_rate=dropout_rate,
                         lr=lr, lamb=lamb, beta=beta, strict = not args.no_strict)
         if args.model == "AAE":
-            model = AAE.load_from_checkpoint(args.ckpt_path, num_domains=num_domains, num_contents=num_contents,
+            model = AAE(num_domains=num_domains, num_contents=num_contents,
                         latent_size=latent_size, lr=lr, depth=depth, 
                         out_channels=out_channels, kernel_size=kernel_size, activation=activation,
                         downsampling=downsampling, upsampling=upsampling, dropout=dropout,
-                        batch_norm=batch_norm, strict = not args.no_strict)
+                        batch_norm=batch_norm)
+            current_model_dict = model.state_dict()
+            loaded_state_dict = torch.load(args.ckpt_path)
+            new_state_dict={k:v if v.size()==current_model_dict[k].size()  else  current_model_dict[k] for k,v in zip(current_model_dict.keys(), loaded_state_dict.values())}
+            model.load_state_dict(new_state_dict, strict=False)
+            # model = AAE.load_from_checkpoint(args.ckpt_path, num_domains=num_domains, num_contents=num_contents,
+            #             latent_size=latent_size, lr=lr, depth=depth, 
+            #             out_channels=out_channels, kernel_size=kernel_size, activation=activation,
+            #             downsampling=downsampling, upsampling=upsampling, dropout=dropout,
+            #             batch_norm=batch_norm, strict = not args.no_strict)
         if args.model == "GAN":
             model = GAN.load_from_checkpoint(args.ckpt_path, num_domains=num_domains, num_contents=num_contents,
                         latent_size=latent_size, lr=lr, depth=depth, 
