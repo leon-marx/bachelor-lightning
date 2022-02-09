be3507d39afac245b466ae174d8a94aed6a49bd1
leon-marx <gooogleon@gmail.com>
2022-02-09 10:25:25 +0100

    added last.ckpt and all generated images to logger

--- tools/train.py
+++ tools/train.py
@@ -337,7 +337,7 @@ if __name__ == "__main__":
     # Callbacks
     callbacks = [
         Logger(args.output_dir, log_dm, train_batch, val_batch, domains, contents, images_on_val=True),
-        pl.callbacks.ModelCheckpoint(monitor="val_loss"),
+        pl.callbacks.ModelCheckpoint(monitor="val_loss", save_last=True),
     ]
     if args.model not in ["AAE", "AAE_v2", "GAN"]:
         callbacks.append(pl.callbacks.stochastic_weight_avg.StochasticWeightAveraging(swa_epoch_start=5))
