9ef36eedd08c28faa46311f36fc90a4457c19bea
leon-marx <gooogleon@gmail.com>
2022-02-15 20:42:23 +0100

    increased warmup-freq back to 10

--- callbacks/logger.py
+++ callbacks/logger.py
@@ -38,7 +38,7 @@ class Logger(Callback):
         self.content_dict = {content: torch.LongTensor([i]) for i, content in enumerate(self.contents)}
 
         self.epoch_counter = -1
-        self.warumup_freq = 1
+        self.warumup_freq = 10
         self.iov_flag = False
         self.images_on_val = images_on_val
 
