d5b267b5ef779a365742dc1f47ac23caa5aa8e40
leon-marx <gooogleon@gmail.com>
2022-02-17 00:38:48 +0100

    small changes to logger

--- callbacks/logger.py
+++ callbacks/logger.py
@@ -75,6 +75,14 @@ class Logger(Callback):
             self.log_umap(trainer, pl_module)
             if self.image_size == 28:
                 self.check_overfit(trainer, pl_module)
+                if not self.images_on_val:
+                    os.makedirs(f"{self.output_dir}/version_{trainer.logger.version}/images", exist_ok=True)
+                    self.log_reconstructions(trainer, pl_module, tensorboard_log=True)
+                    self.log_generated(trainer, pl_module, tensorboard_log=True)
+                    self.log_domain_transfers(trainer, pl_module, tensorboard_log=True)
+                    self.log_content_transfers(trainer, pl_module, tensorboard_log=True)
+                    self.log_losses(trainer)
+                    self.log_grad_flow(trainer, tensorboard_log=True)
             self.epoch_counter = 0
             if getattr(pl_module, "warmer", None) is not None:
                 pl_module.warmer()
