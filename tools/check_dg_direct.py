3a9ce00d6af3d08973d49870b7d9f29b60062047
leon-marx <gooogleon@gmail.com>
2022-03-09 15:14:26 +0100

    bug fix

--- tools/check_dg_direct.py
+++ tools/check_dg_direct.py
@@ -304,7 +304,7 @@ if __name__ == "__main__":
                                 for i, batch in tqdm(enumerate(iter(dm.test_dataloader()))):
                                     pred = torch.argmax(model(batch[0])).item()
                                     truth = torch.argmax(batch[2]).item()
-                                    print(pred, truth)
+                                    # print(pred, truth)
                                     if pred == truth:
                                         loss = 0.0
                                     else:
